8c75e28b559c9884cbb7ca16fad1d54b12ef85b8
8legd <git@8legd.com>
2013-03-03 19:31:39 +0000

    init

---
+++ routing-strategy.js
@@ -0,0 +1,18 @@
+/**
+ * A basic routing strategy that works in node.js and WebPop
+ * (requires ssjs_nodify/nodify.js to be previously loaded in WebPop e.g. in app.js)
+ */
+exports.basic = function(req,res) {
+    try {
+
+        var route = req.path == '/' ? require('./routes/index') : require('./routes' + req.path + '/index');
+        if(route[req.method]) {
+            route[req.method](req,res);
+        } else {
+            console.log('Missing method: ' + req.method + '(req,res) for requested route: extensions/routes' + req.path + '/index.js');
+        }
+    } catch(e) {
+        console.log('Error in route: extensions/routes' + req.path + '/index.js');
+        throw e;
+    }
+}
